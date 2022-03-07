45bf8cd5e1724bf906b299eae2da53d3e54e4a62
mirunasav <100117016+mirunasav@users.noreply.github.com>
2022-03-07 19:56:29 +0200

    problema 2 - mai trebuie facute anumite modificari

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
-#include <bits/stdc++.h>
-#include "Math.h"
+#include <iostream>
+#include "Canvas.h"
+#include <cstring>
 using namespace std;
-
 int main()
 {
-    int a=10, b=20, c=30;
-    double x=10.5, y=20.5, z=30.6;
-    char *p="123", *pp ="475";
 
-    cout<<Math::Add(a, b)<<"\n";
-    cout<<Math::Add(a, b,c)<<"\n";
-    cout<<Math::Add(x, y)<<"\n";
-    cout<<Math::Add(x, y, z)<<"\n";
-    cout<<Math::Mul(a, b)<<"\n";
-    cout<<Math::Mul(a, b,c)<<"\n";
-    cout<<Math::Mul(x, y)<<"\n";
-    cout<<Math::Mul(x, y, z)<<"\n";
-    cout<<Math::Add(4, a, b, c, 12)<<"\n";
-    cout<<Math:: Add(p, pp)<<"\n";
+    Canvas c(30,30);
+    c.Clear();
+    /*c.DrawLine(1, 1, 9, 9, '$');
+    c.Print();
+    c.Clear();*/
+    c.DrawCircle(12, 12, 2, '%');
+    c.Print();
+    c.Clear();
+    c.FillCircle(12, 12, 2, '^');
+    c.Print();
+    c.Clear();
+    c.SetPoint(20, 15, '%');
+    c.Print();
+    c.Clear();
+    c.DrawRect(12, 12, 28, 28, '#' );
+    c.Print();
+    c.Clear();
+    c.FillRect(12, 12, 28, 28, '@');
+    c.Print();
+    c.Clear();
+
 
     system("pause");
     return 0;
